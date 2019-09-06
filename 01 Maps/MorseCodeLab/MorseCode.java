5056ebd8e0da2f6b21f26602098422fbbac5a773
Nathan Chen3 <800018542@fjuhsd.org>
2019-09-06 12:22:11 -0700

    Morse debugging

--- 01 Maps/MorseCodeLab/MorseCode.java
+++ 01 Maps/MorseCodeLab/MorseCode.java
@@ -54,6 +54,7 @@ public class MorseCode{
         addSymbol('.', ".-.-.-");
         addSymbol(',', "--..--");
         addSymbol('?', "..--..");
+        traverse(decodeTree,"");
     }
 
     /**
@@ -73,15 +74,33 @@ public class MorseCode{
     }
 
     private static void insert(char c, String s, TreeNode n){
-        if(n==null)
+        System.out.print("On my quest to insert '"+c+"', ");
+        System.out.println("currently I have '"+s+"' left");
+        try{
+            System.out.println("(BTW, this node has a '"+n.getValue()+"')");
+        }
+        catch(Exception e){
+            System.out.println("I tried to find a value at this node but it gave me a "+e);
+        }
+        if(n==null){
+            System.out.print("This node is null, therefore I ");
             n=new TreeNode(' ');
-        if(s.length()==0)
+            System.out.println("gave null node a '"+n.getValue()+"'");
+        }
+        if(s.length()==0){
+            System.out.print("There are no more characters to traverse, so I");
             n.setValue(c);
+            System.out.println(" gave this node a '"+n.getValue()+"'");
+        }
         else{
-            if(s.charAt(0)==DOT)
+            if(s.charAt(0)==DOT){
+                System.out.println("My map tells me to go LEFT at the DOT");
                 insert(c,s.substring(1),n.getLeft());
-            if(s.charAt(0)==DASH)
+            }
+            if(s.charAt(0)==DASH){
+                System.out.println("My map tells me to go RIGHT at the DASH");
                 insert(c,s.substring(1),n.getRight());
+            }
         }
     }
 
@@ -109,30 +128,40 @@ public class MorseCode{
     }
 
     public static String find(String m, TreeNode n){
-        System.out.println("Finding "+m);
+        System.out.println("Looking at "+m);
+        try{
+            System.out.println("There is a '"+n.getValue()+"' in this pos of the tree");
+        }
+        catch(Exception e){
+            System.out.println("No value here because "+e);
+        }
         if(m.length()==0){
             System.out.println("I found a "+n.getValue());
             return ""+n.getValue();
         }
-        System.out.println("Examining "+m.charAt(0));
-        if(m.charAt(0)==DOT)
+        System.out.println("Node L/R: "+m.charAt(0));
+        if(m.charAt(0)==DOT){
+            System.out.println("Heading left because there is a DOT");
             return find(m.substring(1),n.getLeft());
-        if(m.charAt(0)==DASH)
+        }
+        if(m.charAt(0)==DASH){
+            System.out.println("Heading right because there is a DASH");
             return find(m.substring(1),n.getRight());
+        }
         System.out.println("unreachable statement");
         return " ";
     }
 
     public static void tester(){
         start();
-        //traverse(decodeTree,"");
+        traverse(decodeTree,"");
         System.out.println(find(".-",decodeTree));
         System.out.println(find("..-",decodeTree));
         System.out.println(find(" ",decodeTree));
     }
 
     private static void traverse(TreeNode n,String s){
-        if(n.getValue()==null)
+        if(n==null)
             return;
         traverse(n.getLeft(),s+".");
         System.out.println(s+n.getValue());
