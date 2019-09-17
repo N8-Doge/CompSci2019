cba16fe739844ca8d1ba49b584a4aacd22fedc87
Nathan Chen3 <800018542@fjuhsd.org>
2019-09-16 18:00:39 -0700

    Mystery

--- Programming days/Mystery Programs/mystery Lab I/MysterySolverI.java
+++ Programming days/Mystery Programs/mystery Lab I/MysterySolverI.java
@@ -1,65 +1,68 @@
 import java.util.*;
 
 /*    for all methods - you get 
-    StringTokenizer strTok = new StringTokenizer(input);
-    Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object  // no collections or List or ....
+StringTokenizer strTok = new StringTokenizer(input);
+Stack<?????> s = new Stack<??????>();
+Queue<?????> q = new LinkedList<?????>();
+plus one other Object  // no collections or List or ....
 
-  	plus a String only to return the result
-*/  	
-
+plus a String only to return the result
+ */     
 
 public class MysterySolverI implements MysterySolverInterfaceI
 {
 
-  public String mystery1(String input)
-  {
-  	StringTokenizer strTok = new StringTokenizer(input);
-  	Stack<String> s = new Stack<String>();
-  	Queue<Integer> q = new LinkedList<Integer>();
-  	String str = new String();
-  	
-  	return str;
-  }
-  
-  public String mystery2(String input)
-  {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }
+    public String mystery1(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        Stack<String> s = new Stack<String>();
+        String str = new String();
+        while(strTok.hasMoreTokens())
+            s.push(strTok.nextToken());
+        while(!s.empty())
+            str+=s.pop()+" ";
+        return str.substring(0,str.length()-1);
+    }
 
-  public String mystery3(String input)
-  {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }
+    public String mystery2(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        /*  Stack<?????> s = new Stack<??????>();
+        Queue<?????> q = new LinkedList<?????>();
+        plus one other Object
+         */     
+        return "";
+    }
 
-  public String mystery4(String input)
-  {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }
+    public String mystery3(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        Stack<Integer> s = new Stack<Integer>();
+        String str = new String();
+        while(strTok.hasMoreTokens())
+            s.push((Integer)Math.pow(Integer.parseInt(strTok.nextToken()),2));
+        while(!s.empty())
+            str+=Integer.toString(s.pop())+" ";
+        return str.substring(0,str.length()-1);
+    }
 
-   public String mystery5(String input)
-   {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }
+    public String mystery4(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        /*  Stack<?????> s = new Stack<??????>();
+        Queue<?????> q = new LinkedList<?????>();
+        plus one other Object
+         */     
+        return "";
+    }
+
+    public String mystery5(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        /*  Stack<?????> s = new Stack<??????>();
+        Queue<?????> q = new LinkedList<?????>();
+        plus one other Object
+         */     
+        return "";
+    }
 }
