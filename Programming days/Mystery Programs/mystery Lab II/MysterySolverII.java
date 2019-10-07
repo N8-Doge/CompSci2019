8762a831dbf0a763e4add432477a9576bf18d450
N8-Doge <nathantchennnttcc@gmail.com>
2019-10-06 21:18:06 -0700

    Mystery 2

--- Programming days/Mystery Programs/mystery Lab II/MysterySolverII.java
+++ Programming days/Mystery Programs/mystery Lab II/MysterySolverII.java
@@ -29,36 +29,66 @@ public class MysterySolverII implements MysterySolverInterfaceII
     public String mystery7(String input)
     {
         StringTokenizer strTok = new StringTokenizer(input);
-        Queue<Integer> q = new LinkedList<Integer>();
+        Queue<String> q = new LinkedList<String>();
         String str = new String();
         Integer i = new Integer(0);
-        while(strTok.hasMoreTokens())
-            q.add(Integer.parseInt(strTok.nextToken()));
-        return "";
+        while(strTok.hasMoreTokens()){
+            i += Integer.parseInt(strTok.nextToken());
+            for(int n=1;n<i;++n)
+                q.add(Integer.toString(i)+" ");
+            q.add(Integer.toString(i)+"\n");
+        }
+        while(q.size()!=0)
+            str+=q.poll();
+        return str.substring(0,str.length()-1);
     }
 
     public String mystery8(String input)
     {
-        return "";
+        StringTokenizer strTok = new StringTokenizer(input);
+        Stack<Integer> s = new Stack<Integer>();
+        String str = new String();
+        Integer i = new Integer(0);
+        while(strTok.hasMoreTokens())
+            s.push(Integer.parseInt(strTok.nextToken()));
+        while(!s.isEmpty()){
+            i+=s.pop();
+            str+=Integer.toString(i)+" ";
+        }
+        return str.substring(0,str.length()-1);
     }
 
     public String mystery9(String input)
     {
         StringTokenizer strTok = new StringTokenizer(input);
-        /*  Stack<?????> s = new Stack<??????>();
-        Queue<?????> q = new LinkedList<?????>();
-        plus one other Object
-         */     
-        return "";
+        Stack<Integer> s = new Stack<Integer>();
+        Queue<Integer> q = new LinkedList<Integer>();
+        String str = new String();
+        Integer i;
+        while(strTok.hasMoreTokens()){
+            i = Integer.parseInt(strTok.nextToken());
+            if(i%2==1)
+                q.add(i);
+            else
+                s.push(i);
+        }
+        while(q.size()!=0)
+            str+=q.poll()+" ";
+        while(!s.empty())
+            str+=s.pop()+" ";
+        return str.substring(0,str.length()-1);
     }
 
     public String mysteryF(String input)
     {
-        StringTokenizer strTok = new StringTokenizer(input);
-        /*  Stack<?????> s = new Stack<??????>();
-        Queue<?????> q = new LinkedList<?????>();
-        plus one other Object
-         */     
-        return "";
+        Stack<Integer> s = new Stack<Integer>();
+        String str = new String("1 ");
+        Integer i = new Integer(1);
+        s.push(i);
+        for(int n=1;n<Integer.parseInt(input);n++){
+            s.push(i+(i=s.peek()));
+            str+=Integer.toString(i)+" ";
+        }
+        return str.substring(0,str.length()-1);
     }
 }
--- Programming days/Mystery Programs/mystery Lab III/MysterySolver4AB.java
+++ Programming days/Mystery Programs/mystery Lab III/MysterySolver4AB.java
@@ -1,99 +1,99 @@
 import java.util.*;
 
 /*    for all methods - you get 
-    StringTokenizer strTok = new StringTokenizer(input);
-    Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
+StringTokenizer strTok = new StringTokenizer(input);
+Stack<?????> s = new Stack<??????>();
+Queue<?????> q = new LinkedList<?????>();
+plus one other Object
 
-  	plus a String only to return the result
-*/  	
+plus a String only to return the result
+ */  	
 
 public class MysterySolver4AB implements MysterySolverInterface4AB
 {
-/*
- *   required operators for PreFix and Post are:
- *     *, /, +, -, % and ^ (exponent)
- *     
- *     You may assume all inputs are integers,
- *     all calculations should done using integer Math  (5 / 2 == 2)
- */
-  public String mysteryPreFix(String input)
-  {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }
+    /*
+     *   required operators for PreFix and Post are:
+     *     *, /, +, -, % and ^ (exponent)
+     *     
+     *     You may assume all inputs are integers,
+     *     all calculations should done using integer Math  (5 / 2 == 2)
+     */
+    public String mysteryPreFix(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        /*  Stack<?????> s = new Stack<??????>();
+        Queue<?????> q = new LinkedList<?????>();
+        plus one other Object
+         */  	
+        return "";
+    }
 
-  public String mysteryPostFix(String input)
-  {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }
-  	
-  public String mysteryP(String input)
-  {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }
-  
-   public String mysteryS(String input)
-   {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }  
+    public String mysteryPostFix(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        /*  Stack<?????> s = new Stack<??????>();
+        Queue<?????> q = new LinkedList<?????>();
+        plus one other Object
+         */  	
+        return "";
+    }
 
-/*
- *    See tester for sample input - output
- *    
- *    You may assume the first int < second int.
- *    
- *    You may assume all three ints will be greater than 0
- */  
-  
-  public String mystery10(String input)
-  {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }
+    public String mysteryP(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        /*  Stack<?????> s = new Stack<??????>();
+        Queue<?????> q = new LinkedList<?????>();
+        plus one other Object
+         */  	
+        return "";
+    }
 
-/*
- *    Precondition:  The tester will never have more than 3 consecutive numbers
- *                   That is, the same number will never appear more than 3 times in a row
- *                   
- *    restating the precondition
- *
- *    you may assume no input will have more than 3 consecutive digits
- *    that is:  3333, 211116, 10000000, 33334444 are all invalid values for the first part of the input
- *    but:       123456, 300044412, ... are valid values for the first part of input
- *    
- */  
-  public String mysteryC(String input)
-  {
-  	StringTokenizer strTok = new StringTokenizer(input);
-/*  Stack<?????> s = new Stack<??????>();
-  	Queue<?????> q = new LinkedList<?????>();
-  	plus one other Object
-*/  	
-  	return "";
-  }
+    public String mysteryS(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        /*  Stack<?????> s = new Stack<??????>();
+        Queue<?????> q = new LinkedList<?????>();
+        plus one other Object
+         */  	
+        return "";
+    }  
+
+    /*
+     *    See tester for sample input - output
+     *    
+     *    You may assume the first int < second int.
+     *    
+     *    You may assume all three ints will be greater than 0
+     */  
+
+    public String mystery10(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        /*  Stack<?????> s = new Stack<??????>();
+        Queue<?????> q = new LinkedList<?????>();
+        plus one other Object
+         */  	
+        return "";
+    }
+
+    /*
+     *    Precondition:  The tester will never have more than 3 consecutive numbers
+     *                   That is, the same number will never appear more than 3 times in a row
+     *                   
+     *    restating the precondition
+     *
+     *    you may assume no input will have more than 3 consecutive digits
+     *    that is:  3333, 211116, 10000000, 33334444 are all invalid values for the first part of the input
+     *    but:       123456, 300044412, ... are valid values for the first part of input
+     *    
+     */  
+    public String mysteryC(String input)
+    {
+        StringTokenizer strTok = new StringTokenizer(input);
+        /*  Stack<?????> s = new Stack<??????>();
+        Queue<?????> q = new LinkedList<?????>();
+        plus one other Object
+         */  	
+        return "";
+    }
 }
