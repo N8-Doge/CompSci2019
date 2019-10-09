dc385cbe4fdeab6f7f4b22c7e3c61d8ce88dda63
Nathan Chen3 <800018542@fjuhsd.org>
2019-10-09 12:22:04 -0700

    todo updated

--- 03 Functions/problem 107, page 134 student/Problem107Page134.java
+++ 03 Functions/problem 107, page 134 student/Problem107Page134.java
@@ -9,37 +9,55 @@ import java.lang.Math;
  * @author  
  * @version (a version number or a date)
  */
-public class Problem107Page134
-{
-    /*
-     *  returns an array of all months with a Friday the 13th in Calendar order
-     *        Use the following Strings for each Month
-     *        "Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"
-     */
+public class Problem107Page134{
     public static String[] findFriday13(int year){
         double x = (double)year;
         int y = (int)((x+(x-1.0)/4.0-(x-1.0)/100.0+(x-1.0)/400.0)%7.0);
         boolean b = (year%4==0)&&(year%100!=0||year%400==0);
         switch(y){
             case 0:
-            if(b)
+            if(!b)
                 return m(new int[]{1,10});
             else
-                return m(new int[]{1,5,7});
+                return m(new int[]{1,4,7});
             case 1:
-            if(b)
-                return m(new int[]{5,7});
+            if(!b)
+                return m(new int[]{4,7});
             else
                 return m(new int[]{9,12});
+            case 2:
+            if(!b)
+                return m(new int[]{9,12});
+            else
+                return m(new int[]{6});
+            case 3:
+            if(!b)
+                return m(new int[]{6});
+            else
+                return m(new int[]{3,11});
+            case 4:
+            if(!b)
+                return m(new int[]{2,3,11});
+            else
+                return m(new int[]{2,8});
+            case 5:
+            if(!b)
+                return m(new int[]{8});
+            else
+                return m(new int[]{5});
+            default:
+            if(!b)
+                return m(new int[]{5});
+            else
+                return m(new int[]{10});
         }
-        return new String[1];
     }
     
     public static String[] m(int[] months){
         String[] s = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
         String[] r = new String[months.length];
         for(int i=0;i<months.length;++i){
-            r[i]=s[months[i]];
+            r[i]=s[months[i]-1];
         }
         return r;
     }
