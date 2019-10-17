6f721e443081438fbb6fd810b10e13f9b451b44f
Nathan Chen3 <800018542@fjuhsd.org>
2019-10-17 12:27:05 -0700

    man

--- Programming days/Mantissa Exponent/Mantissa Exponent/MatissaExponentConverter.java
+++ Programming days/Mantissa Exponent/Mantissa Exponent/MatissaExponentConverter.java
@@ -1,31 +1,35 @@
 import java.util.*;
 public class MatissaExponentConverter{
+    //.005 double accuracy
     public static String toMatissaExponent(double val, int m, int e){
         int x = (val==0.)? 0 : Math.getExponent(val)+1;
-        String ex = Integer.toString(x,2);
-        if(ex.length()>=e)
+        String ex = Integer.toString(Math.abs(x),2);
+        while(ex.length()<e)
+            ex="0"+ex;
+        if(x<0.)
+            ex.replace('1','x').replace('0','1').replace('x','0');
+        if(ex.length()>e)
             return "np";
-        double v = Math.abs(val);
-        double b = 1;
+        System.out.println("exponent is "+ex);
         String mn = "";
-        while(b<v)
-            b*=2;
-        while(v>=0.01){
-            b/=2;
-            if(v>=b){
+        double d = 1;
+        double v = Math.abs(val);
+        while(d<val)
+            d*=2;
+        for(int i=0;i<m;++i){
+            if(d<=v){
                 mn+="1";
-                v-=b;
+                v-=d;
             }
             else
                 mn+="0";
+            d/=2;
         }
-        if(mn.length()>m)
+        System.out.println("v is "+v);
+
+        //code negative nums IDIOT
+        if(v>.05)
             return "np";
-        while(mn.length()<m-1)
-            mn+="0";
-        while(ex.length()<e)
-            ex="0"+ex;
-        mn = (val<0.)? "1"+mn : "0"+mn;
         return mn+" "+ex;
     }
 
@@ -34,14 +38,19 @@ public class MatissaExponentConverter{
         String k = str.substring(str.indexOf(" ")+1);
         double m=0.,d=0.;
         for(int i=0;i<j.length();++i){
-            if(i==0 && j.charAt(0)=='1')
-                d-=1.;
-            else if(j.charAt(i)=='1')
-                d+=1./Math.pow(2.,i);
+            if(j.charAt(i)=='1'){
+                if(i==0)
+                    d-=1.;
+                else
+                    d+=1./Math.pow(2.,i);
+            }
         }
         for(int i=0;i<k.length();++i)
             if(k.charAt(k.length()-i-1)=='1')
-                m+=Math.pow(2,i);
+                if(k.length()-i-1==0)
+                    m-=Math.pow(2,i);
+                else
+                    m+=Math.pow(2,i);
         return Math.pow(2,m)*d;
     }
 }
