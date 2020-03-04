22316ffa20cf3e5679663a69d638d93fe9d80546
N8-Doge <nathantchennnttcc@gmail.com>
2020-03-03 22:00:17 -0800

    mystery3

--- Programming days/Mystery Programs/mystery Lab III/MysterySolver4AB.java
+++ Programming days/Mystery Programs/mystery Lab III/MysterySolver4AB.java
@@ -1,82 +1,98 @@
 import java.util.*;
+public class MysterySolver4AB implements MysterySolverInterface4AB{
+    public String mysteryPreFix(String exprsn){
+        exprsn = exprsn.replaceAll(" ", "");
+        Stack<Double> s = new Stack<Double>(); 
+        for (int j = exprsn.length()-1; j >= 0; j--){ 
+            if (isOperand(exprsn.charAt(j))){
+                System.out.println( (double)(exprsn.charAt(j) - 48));
+                s.push((double)(exprsn.charAt(j) - 48)); 
+            }
 
-/*    for all methods - you get 
-StringTokenizer strTok = new StringTokenizer(input);
-Stack<?????> s = new Stack<??????>();
-Queue<?????> q = new LinkedList<?????>();
-plus one other Object
+            else{ 
+                double o1 = s.peek(); 
+                s.pop(); 
+                double o2 = s.peek(); 
+                s.pop();
+                switch (exprsn.charAt(j)){ 
+                    case '+': 
+                    s.push(o1 + o2); 
+                    break; 
+                    case '-': 
+                    s.push(o1 - o2); 
+                    break; 
+                    case '^': 
+                    s.push(Math.pow(o1,o2)); 
+                    break; 
+                    case '*': 
+                    s.push(o1 * o2); 
+                    break; 
+                    case '%': 
+                    s.push(o1 % o2); 
+                    break; 
+                    case '/': 
+                    s.push(o1 / o2); 
+                    break; 
 
-plus a String only to return the result
- */     
-
-public class MysterySolver4AB implements MysterySolverInterface4AB
-{
-    /*
-     *   required operators for PreFix and Post are:
-     *     *, /, +, -, % and ^ (exponent)
-     *     
-     *     You may assume all inputs are integers,
-     *     all calculations should done using integer Math  (5 / 2 == 2)
-     */
-    public String mysteryPreFix(String input)
-    {
-        StringTokenizer strTok = new StringTokenizer(input);
-        /*  Stack<?????> s = new Stack<??????>();
-        Queue<?????> q = new LinkedList<?????>();
-        plus one other Object
-         */     
-        return "";
+                } 
+            } 
+        } 
+        int x = (int) Math.round(s.peek());
+        return "" + x; 
     }
+
+    public static Boolean isOperand(char c){ 
+        return (c >= 48 && c <= 57);
+    } 
 
     public String mysteryPostFix(String input)
     {
-        StringTokenizer strTok = new StringTokenizer(input);
-        /*  Stack<?????> s = new Stack<??????>();
-        Queue<?????> q = new LinkedList<?????>();
-        plus one other Object
-         */     
         return "";
     }
 
-    public String mysteryP(String input)
-    {
-        StringTokenizer strTok = new StringTokenizer(input);
-        /*  Stack<?????> s = new Stack<??????>();
-        Queue<?????> q = new LinkedList<?????>();
-        plus one other Object
-         */     
-        return "";
+    public String mysteryP(String input){
+        StringTokenizer strTok=new StringTokenizer(input);
+        Stack<Integer> stack=new Stack<Integer>();
+        stack.push(2);
+        Queue<Integer> queue=new LinkedList<Integer>();
+        Integer i=Integer.parseInt(strTok.nextToken());
+        String ans="2 ";
+        while(i>1){
+            queue.add(stack.peek());
+            stack.push(stack.pop()+1);
+            while(queue.peek()!=stack.peek()-1){
+                if(stack.peek()%queue.peek()==0)
+                    break;
+                queue.add(queue.poll());
+            }
+            if(stack.peek()%queue.peek()!=0){
+                ans+=""+stack.peek()+" ";
+                i--;
+            }
+        }
+        return ans.substring(0,ans.length()-1);
     }
 
     public String mysteryS(String input)
     {
+
         StringTokenizer strTok = new StringTokenizer(input);
-        Queue<Integer> q = new LinkedList<Integer>();
-        String str = new String();
-        Integer i = Integer.parseInt(strTok.nextToken());
-        q.add(i);
-        while(strTok.hasMoreTokens()){
-            int n = Integer.parseInt(strTok.nextToken());
-            if(n<i)
-                i=n;
-            boolean b = true;
-            while(b){
-                if(n<q.peek()){
-                    q.add(n);
-                    b=false;
-                }
-                else
-                    q.add(q.poll());
-            }
-            System.out.println("I have successfully added "+n);
-            while(q.peek()!=i)
-                q.add(q.poll());
-            System.out.println("Reset order");
+        Stack<String> s = new Stack<String>();
+        Queue<String> q = new LinkedList<String>();
+        String ans = "";
+        ArrayList<Integer>elements=new ArrayList<Integer>();
+        while(strTok.hasMoreTokens())
+        {
+            elements.add(Integer.parseInt(strTok.nextToken()));
         }
-        while(q.size()!=0){
-            str += Integer.toString(q.poll())+" ";
+        Collections.sort(elements);
+
+        for(int i = 0; i < elements.size(); i++)
+        {
+            ans = ans + " " + elements.get(i);
         }
-        return str.substring(0,str.length()-1);
+
+        return ans.substring(1);
     }  
 
     /*
@@ -89,32 +105,55 @@ public class MysterySolver4AB implements MysterySolverInterface4AB
 
     public String mystery10(String input)
     {
+        String ans = "";
         StringTokenizer strTok = new StringTokenizer(input);
-        /*  Stack<?????> s = new Stack<??????>();
-        Queue<?????> q = new LinkedList<?????>();
-        plus one other Object
-         */     
-        return "";
+        Stack<String> s = new Stack<String>();
+        Queue<String> q = new LinkedList<String>();
+
+        double frac = Double.valueOf(strTok.nextToken())/Double.valueOf(strTok.nextToken());
+        ans = ans + frac + "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+        //int x= Integer.parseInt(strTok.nextToken());
+        ans = ans.substring(2);
+        ans = ans.substring(0, Integer.parseInt(strTok.nextToken()));
+        //System.out.println(ans);
+        return ans;
     }
 
-    /*
-     *    Precondition:  The tester will never have more than 3 consecutive numbers
-     *                   That is, the same number will never appear more than 3 times in a row
-     *                   
-     *    restating the precondition
-     *
-     *    you may assume no input will have more than 3 consecutive digits
-     *    that is:  3333, 211116, 10000000, 33334444 are all invalid values for the first part of the input
-     *    but:       123456, 300044412, ... are valid values for the first part of input
-     *    
-     */  
-    public String mysteryC(String input)
+    public String mysteryC(String number)
     {
-        StringTokenizer strTok = new StringTokenizer(input);
-        /*  Stack<?????> s = new Stack<??????>();
-        Queue<?????> q = new LinkedList<?????>();
-        plus one other Object
-         */     
-        return "";
+        StringTokenizer strTok = new StringTokenizer(number);
+        String currentIteration = strTok.nextToken();
+        String ans = "OUTPUT:\n" + currentIteration;
+
+        int x = Integer.parseInt(strTok.nextToken());
+        strTok = new StringTokenizer(number);
+
+        for(int i = 0; i<x; i++)
+        {
+            currentIteration = lockAndSaySequence(currentIteration);
+            ans = ans + "\n" + currentIteration;
+        }
+
+        return ans;
+    }
+
+    public String lockAndSaySequence(String number)
+    {
+        StringBuilder result= new StringBuilder();
+        char repeat= number.charAt(0);
+        number= number.substring(1) + " ";
+        int times= 1;
+
+        for(char actual: number.toCharArray()){
+            if(actual != repeat){
+                result.append(times + "" + repeat);
+                times= 1;
+                repeat= actual;
+            }else{
+                times+= 1;
+            }
+        }
+        return result.toString();
     }
 }
